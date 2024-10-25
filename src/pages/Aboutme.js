d87b3c699d1d2929297d4b2f7699fe5b4f2218b7
Mariana Sarovic <sarovicmariana@gmail.com>
2024-10-26 01:23:40 +0200

    about me, foto, footer

--- src/pages/Aboutme.js
+++ src/pages/Aboutme.js
@@ -1,7 +1,26 @@
+// import imgOmne1 from '../imgomne/img-omne-1.jpg';
+
 const Aboutme = () => {
   return (
-    <section>
-      <p>Uvodni strana About me</p>
+    <section className="aboutme">
+      <div className="aboutme-text">
+        <h1>- O mně -</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+          quod sapiente corporis sint quibusdam quidem asperiores id impedit
+          exercitationem soluta expedita cum accusantium sunt, veniam illum? Ad
+          aliquid earum unde!
+        </p>
+
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+          aperiam, voluptate sapiente dolorem eaque facere quia fugit repellat
+          iste voluptas, sit debitis modi iure, harum recusandae non. Neque,
+          dolor nam.
+        </p>
+      </div>
+      {/* <img className="aboutme-image" src={imgOmne1} alt="Popis obrázka" />; */}
+      <div className="aboutme-background-image"></div>
     </section>
   );
 };

610e77669619fce242d87b335bb08e92528b43d4
Mariana Sarovic <sarovicmariana@gmail.com>
2024-10-31 21:35:27 +0100

    naimportovana ./i18n do index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import './i18n'; // Importujte i18n konfiguráciu
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>

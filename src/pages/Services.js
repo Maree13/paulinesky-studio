60e297a31164907d92aaa5ff581ddf82baa8b3b2
Mariana Sarovic <sarovicmariana@gmail.com>
2024-11-12 12:22:36 +0100

    cesko anglicka verzia Services, odpojena komponenta ServiceData.js lebo data su v jsonu

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,22 +1,31 @@
+import React from 'react';
 import ReservationButton from '../components/ReservationButton';
 import { useTranslation } from 'react-i18next';
-import ServicesData from '../components/ServicesData';
+// Importing JSON data from the correct paths
+import ServicesDataEn from '../locales/en/services.json';
+import ServicesDataCs from '../locales/cs/services.json';
 
 const Services = () => {
-  const { t } = useTranslation('services');
+  const { t, i18n } = useTranslation('services');
+
+  // Determine which language dataset to use based on the current language
+  const ServicesData = i18n.language.startsWith('cs')
+    ? ServicesDataCs
+    : ServicesDataEn;
 
   return (
     <section className="services">
       <div className="services-text">
         <h1 className="services-title">{t('servicesTitle')}</h1>
-        {ServicesData.map((category) => (
-          <div key={category.category}>
-            <h2>{category.category}</h2>
+        {ServicesData.categories.map((category, idx) => (
+          <div key={idx}>
+            <h2>{t(category.category)}</h2>
             <table>
               <tbody>
                 {category.services.map((service, index) => (
                   <tr key={index}>
-                    <td>{service.name}</td>
+                    <td>{t(service.name)}</td>{' '}
+                    {/* Optionally translate service names if needed */}
                     <td>{service.price}</td>
                   </tr>
                 ))}
@@ -24,10 +33,8 @@ const Services = () => {
             </table>
           </div>
         ))}
-
         <ReservationButton />
       </div>
-
       <div className="services-background-image"></div>
     </section>
   );

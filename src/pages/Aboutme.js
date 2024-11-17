4a9e1bb6f29e4752d3d40b53fb8735a9cb96c16a
Mariana Sarovic <sarovicmariana@gmail.com>
2024-11-17 22:59:52 +0100

    vymazana ja z kontaktov

--- src/pages/Aboutme.js
+++ src/pages/Aboutme.js
@@ -12,19 +12,7 @@ const Aboutme = () => {
         <p className="paragraph aboutme-paragraph">{t('paragraph1')}</p>
         <p className="paragraph aboutme-paragraph">{t('paragraph2')}</p>
         <p className="paragraph aboutme-paragraph">{t('paragraph3')}</p>
-        {/* <h1>- O mně -</h1>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis,
-          quod sapiente corporis sint quibusdam quidem asperiores id impedit
-          exercitationem soluta expedita cum accusantium sunt, veniam illum? Ad
-          aliquid earum unde!
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates
-          aperiam, voluptate sapiente dolorem eaque facere quia fugit repellat
-          iste voluptas, sit debitis modi iure, harum recusandae non. Neque,
-          dolor nam.
-        </p> */}
+
         <ReservationButton />
       </div>
       {/* Obrázek na pozadí s dynamickou změnou */}

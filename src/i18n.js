2b5ebe81b2933f0ef2db7e65f1ba344fe6197d30
Mariana Sarovic <sarovicmariana@gmail.com>
2024-11-23 17:54:41 +0100

    Uprava pre preklad URL, zatim preklad funguje URL este ne

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,7 @@
 // src/i18n.js
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 // Importujte preklady
 import translationEN from './locales/en/translation.json';
@@ -15,6 +16,8 @@ import introCS from './locales/cs/intro.json';
 import introEN from './locales/en/intro.json';
 import servicesCS from './locales/cs/services.json';
 import servicesEN from './locales/en/services.json';
+import routesEN from './locales/en/routes.json';
+import routesCS from './locales/cs/routes.json';
 
 // Definujte dostupné jazyky a preklady
 const resources = {
@@ -25,6 +28,7 @@ const resources = {
     contact: contactEN,
     intro: introEN,
     services: servicesEN,
+    routes: routesEN,
   },
   cs: {
     translation: translationCS,
@@ -33,25 +37,53 @@ const resources = {
     contact: contactCS,
     intro: introCS,
     services: servicesCS,
+    routes: routesCS,
   },
 };
 
-// Inicializujte i18n
+// porofnaci kod:
+
 i18n
+  .use(LanguageDetector) // Detekcia jazyka
   .use(initReactI18next) // Prepojenie s Reactom
   .init({
     resources,
-    lng: 'cs', // Počiatočný jazyk
-    fallbackLng: 'en', // Jazyk, ktorý sa použije, ak preklad chýba
+    fallbackLng: 'en',
+    debug: false, // Nastavte na true pre debugging
 
-    ns: ['translation', 'navbar', 'aboutme', 'contact', 'intro', 'services'], // Definujte použité namespaces
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    },
+
+    ns: ['translation', 'routes', 'navbar', 'aboutme', 'services', 'intro'],
     defaultNS: 'translation',
 
-    keySeparator: false, // Používajte priamo kľúče pre preklady
+    keySeparator: false,
 
     interpolation: {
-      escapeValue: false, // React už escapuje XSS
+      escapeValue: false,
     },
   });
 
 export default i18n;
+
+// Inicializujte i18n
+// i18n
+//   .use(initReactI18next)
+//   .init({
+//     resources,
+//     lng: 'cs',
+//     fallbackLng: 'en',
+
+//     ns: ['translation', 'navbar', 'aboutme', 'contact', 'intro', 'services'],
+//     defaultNS: 'translation',
+
+//     keySeparator: false,
+
+//     interpolation: {
+//       escapeValue: false,
+//     },
+//   });
+
+// export default i18n;
